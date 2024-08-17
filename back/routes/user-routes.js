d795a542a76e996c78b072ea6121eb008d9e7ea0
Kalyan Mishra <davidkalyan72@gmail.com>
2024-08-17 17:58:54 +0530

    file upload fixed multer

--- back/routes/user-routes.js
+++ back/routes/user-routes.js
@@ -5,17 +5,18 @@ const path = require("path");
 
 //for file upload use multer
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.resolve(`./public/uploads/`));
+    destination: function (req, file, cb) {   
+        return cb(null, path.resolve(`./public/uploads/`)); 
     },
-    filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}-${file.originalname}`);
-    }
-})
+    filename: function (req, file, cb) {
+        return cb(null, `${Date.now()}-${file.originalname}`);   
+    },
+});
 const upload = multer({ storage: storage });
+ 
 const router = Router();
 
-router.post('/signup', upload.single('photo'), signUpController);
+router.post('/signup', upload.single('profilePhoto'), signUpController);
 router.post('/login', loginController);
 
 module.exports = router;
